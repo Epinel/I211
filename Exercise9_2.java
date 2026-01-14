116d2df096b61c886221c58e8b3f50c1a05d6ca2
Liz Pinel <elpouche@iu.edu>
2026-01-14 13:52:34 -0500

    Create Exercise9_2.java

---
+++ Exercise9_2.java
@@ -0,0 +1,47 @@
+public class Exercise9_2{
+    public static void main(String[] args) {
+        Stock stock = new Stock("ORCL","Oracle Corporation");
+        stock.setPreviousClosingPrice(34.5);
+        stock.setCurrentPrice(34.35);
+
+        System.out.println("Previous Closing Price: "+stock.getPreviousClosingPrice());
+        System.out.println("Current Price: "+stock.getCurrentPrice());
+        System.out.println("Percent Change: "+stock.getChangePercent()*100+"%");
+    }
+}
+
+class Stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    public Stock() {
+    }
+
+    public Stock(String newSymbol, String newName) {
+        symbol = newSymbol;
+        name = newName;
+    }
+
+    public double getChangePercent() {
+        return (currentPrice - previousClosingPrice) / previousClosingPrice;
+    }
+
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public double getPreviousClosingPrice() {
+        return previousClosingPrice;
+    }
+
+    public void setCurrentPrice(double newCurrentPrice) {
+        currentPrice = newCurrentPrice;
+    }
+
+    public void setPreviousClosingPrice(double newPreviousClosingPrice) {
+        previousClosingPrice = newPreviousClosingPrice;
+    }
+}
+
