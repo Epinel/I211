0d30728d136b0094bec58fce9df74996aec91703
Liz Pinel <elpouche@iu.edu>
2026-01-14 13:53:42 -0500

    Create Listing 9_4.java

---
+++ Listing 9_4.java
@@ -0,0 +1,17 @@
+public class Listing9_4 {
+    public static void main(String[] args) {
+        Listing9_3 tv1 = new Listing9_3();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
+
+        Listing9_3 tv2 = new Listing9_3();
+        tv2.turnOn();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.VolumeUp();
+
+        System.out.println("tv1's channel is "+tv1.channel+" and volume level is "+tv1.volumeLevel);
+        System.out.println("tv2's channel is "+tv2.channel+" and volume level is "+tv2.volumeLevel);
+    }
+}
