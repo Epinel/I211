993e8ddc7d99f86e7ecccfb549f694f9feabcfea
Liz Pinel <elpouche@iu.edu>
2026-02-27 17:29:55 -0500

    Create Exercise18_2.java

---
+++ Exercise18_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Exercise18_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an index for a Fibonacci number: ");
+        int index = input.nextInt();
+
+        System.out.println("The Fibonacci number at index "+index+" is "+fib(index));
+    }
+
+    public static long fib(int n) {
+        int f0 = 0, f1 = 1, currentFib;
+
+        if (n==0) return 0;
+        if (n==1) return 1;
+
+        for (int i=2; i<=n; i++){
+            currentFib = f0+f1;
+            f0=f1;
+            f1=currentFib;
+        }
+        return f1;
+    }
+}
