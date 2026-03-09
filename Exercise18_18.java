e8aad28790097e6485f3358c5273a9dc874940bc
Liz Pinel <elpouche@iu.edu>
2026-03-09 17:39:03 -0400

    Create Exercise18_18.java

---
+++ Exercise18_18.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercise18_18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of disks: ");
+        int disks = input.nextInt();
+
+        System.out.println("The moves are: ");
+        moveDisks(disks, 'A','B','C');
+
+        System.out.println("The number of moves is "+count);
+    }
+
+    static int count = 0;
+
+    public static void moveDisks(int n, char fromTower, char toTower, char auxTower) {
+        count++;
+        if (n==1)
+        System.out.println("Move disk "+n+" from "+fromTower+" to "+toTower);
+        else {
+            moveDisks(n-1, fromTower, auxTower, toTower);
+            System.out.println("Move disk "+n+" from "+fromTower+" to "+toTower);
+            moveDisks(n-1, auxTower, toTower, fromTower);
+        }
+    }
+}
